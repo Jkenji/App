01ea67ea2329a45ec3c3b6cd2644c2b0ba2105c1
Enzo Kikuchi <127358329+Jkenji@users.noreply.github.com>
2024-09-11 00:22:45 -0300

    fundamentos 004

--- index.js
+++ index.js
@@ -1,42 +1,10 @@
-console.log("hello, world!")
-
-let mensagens = "hello, world!"
-
-const mensagem = "hello, kenji!"
-{
-    const mensagem = "hello, kenji!"
-    console.log(mensagem)
-}
-console.log(mensagem);
-
-let meta01 = ['kenji', 'hi']
-let metas02 = [2, 'kenji']
-
-console.log(metas[1] + ", " + metas[0]) 
-
-
-let meta = {
-    value: 'ler um livro por mês',
-    address: 2,
-    checked: false,
-    log: (info) => {
-        console.log(info)
+const start = () => {
+    let count = 0
+    while(count < 10){
+        console.log(count)
+        count++
+        
     }
-
 }
 
-let metas = [
-    meta,
-    {
-        value: "caminhar com os cachorros todos os dias",
-        checked: false
-    }
-]
-
-meta.log(meta.value)
-console.log(metas[1].value)
-
-//function //arrow function
-const criarMeta = () => {}
-
-//function criarMeta() {}
+start()
